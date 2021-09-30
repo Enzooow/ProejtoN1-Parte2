1081add15ad82daf57cca3e5a5ad14c4aa466346
Enzooow <sabanai.enzo@gmail.com>
2021-09-30 01:51:15 -0300

    Update ProjetoN1_Parte2.py

---
+++ ProjetoN1_Parte2.py
@@ -0,0 +1,12 @@
+from collections import OrderedDict
+
+ListaNomes = {}
+
+print("Evento técnico Anhembi 2021")
+print("1 - Cadastro de novos usuários")
+print("2 - Usuários cadastrados | Ordem de cadastro")
+print("3 - Usuários cadastrados | Ordem alfabética")
+print("4 - Verificação de usuários | Busca por nome")
+print("5 - Remover usuário | Remoção por e-mail")
+print("6 - Alterar nome de usuário cadastrado | Por e-mail")
+int(input("Escolha sua opção: "))
